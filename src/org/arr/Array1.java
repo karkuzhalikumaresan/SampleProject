35ee1869abbb5c3e5ee0635dac003acb8c01d1da
karkuzhalikumaresan <166905409+karkuzhalikumaresan@users.noreply.github.com>
2024-09-26 17:38:26 +0530

    Update Array1.java
    data changes

--- src/org/arr/Array1.java
+++ src/org/arr/Array1.java
@@ -2,7 +2,7 @@ package org.arr;
 
 public class Array1 {
 	public static void main (String []args) {
-		int a[]= new int[]{1,2,3,4,5,6,7,8,9,10};
+		int a[]= new int[]{1,2,3,4,5,66,7,8,99,10};
 		int sum=0;
 		float avg=0.0f;
 		for (int i=0;i<a.length;i++) {
